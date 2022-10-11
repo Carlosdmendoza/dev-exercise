791761f573f58666a4ef936b6dd2080e342a1a77
Carlos David Mendoza Archaga <carlosdmendoza99@gmail.com>
2022-10-11 11:27:02 -0600

    text

---
+++ my-app/src/test.tsx
@@ -0,0 +1,39 @@
+
+
+const rightTask = () => {
+  return (
+    <Container style={{ flexbirection: "column", gap: "Spx" }} padding="
+      <Container padding="0" style={{ gap: "10px" }}»
+        <Icon margin="0" icon={"faArrowlp"} color="successLightest" />
+        <Typography margin="0" variant="h4">
+          2
+        </Typography>
+      </Container>
+      <Typography variant="boldExtrasnall" margin="0" align="center">
+        Last Week
+      </Typography>
+    </Container>
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+                     
+
+                             
+
+     
+
+ 
+
+const rightCompleted = () => {
+    return [[
+    <Container style={{ flexDirection: "column", gap: "Spx" }} padding="
+      <Container padding="" style={{ gap: "10px" }}3
+        <Icon margin="0" icon={"faPlus"} color="successLightest" />
+        <Typography margin="0" variant="hd">
+          16
+        </Typography>
+      </Container>
+      <Typography variant="boldExtrasnall” margin="" align="center">
+        Last Week
+      </Typography>
+    </Container>
